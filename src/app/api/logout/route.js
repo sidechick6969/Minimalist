06038ccdf737a7ea130f0a8dd0a9a2ea409f6145
sidechick6969 <chickside6969@gmail.com>
2026-05-20 02:04:42 +0530

    Delete src directory

--- src/app/api/logout/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-
-export async function POST() {
-  (await cookies()).delete('session');
-  return NextResponse.json({ message: 'Logged out' }, { status: 200 });
-}
